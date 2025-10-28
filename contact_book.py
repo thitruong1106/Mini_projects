c2bc6901bdb3be802d85023f56a8b38c906ef375
thitruong1106 <thitruong1106@gmail.com>
2025-10-28 22:48:26 +1100

    Fix load_contact() reutrn none instead of empty list

--- contact_book.py
+++ contact_book.py
@@ -23,3 +23,65 @@ def view_contacts(contacts):
     #view all saved contacts 
     if not contacts:
         print("\n No contacts Found")
+        return
+    print("\n Contact List:")
+    for idx, contact in enumerate (contacts, start=1):
+        print(f"{idx}, {contact['name']} - {contact['phone']} - {contact['email']}")
+        print()
+
+def add_contacts(contacts):
+    "Add new entry into json file"
+    name = input("Enter the name: \t").strip()
+    phone = input("Enter a phone number: \t").strip()
+    email = input("Enter an email: \t").strip()
+    contacts.append({"name": name, "phone": phone, "email": email})
+    print(f"Contact {name} added successfully.")
+
+def search_contact(contacts):
+    "Search contact by keyword NAME"
+    keyword = input("Enter a name to search: \n").strip().lower()
+    results = [c for c in contacts if keyword in c['name'].lower()]
+    if results: 
+        print("\nSearch Results:")
+        for c in results:
+            print(f"- {c['name']} | {c['phone']} | {c['email']}")
+        else:
+            print("No contact has been found")
+
+def delete_contacts(contacts):
+    "Delete by name"
+    name = input("Enter the name of the contact to be deleted: \t").strip()
+    for c in contacts: 
+        if c[name].lower() == name.lower(): #if contact name matches input
+            contacts.remove(c)
+            save_contacts(contacts)
+            print(f"{name} contact has been removed succesfully.")
+            return 
+        print("Contact is not Found.")
+
+def main():
+    contact = load_contacts()
+    while True:
+        print("Welcome to Contact Book")
+        print("1. Load Contact")
+        print("2. Add contact")
+        print("3. Search Contact")
+        print("4. Delete Contact")
+        print("5. Exit")
+
+        choice = input("Choose option (1 - 5)")
+        if choice == "1":
+            view_contacts(contact)
+        elif choice == "2":
+            add_contacts(contact)
+        elif choice == "3":
+            search_contact(contact)
+        elif choice == "4":
+            delete_contacts(contact)
+        elif choice == "5":
+            break
+        else:
+            print("Invalid input, Enter a valid choice (1 - 5)")
+
+if __name__ == "__main__":
+    main()
